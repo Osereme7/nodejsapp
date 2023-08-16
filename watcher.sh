cb27faecd63a34ff69b1562008d052e5e6383b58
Osereme  Okojie <ehisojieokojie7@gmail.com>
2023-08-16 17:03:08 +0100

    updated

--- watcher.sh
+++ watcher.sh
@@ -1,12 +1,16 @@
+SERVER_URL="http://nodejsapp@aws.com"
+EXPECTED_VERSION="1.0.0"
+
 check_server() {
   RESPONSE=$(curl -s "$SERVER_URL")
   if [[ "$RESPONSE" == *"$EXPECTED_VERSION"* ]]; then
-    echo "Server is up and serving the expected version: $EXPECTED_VERSION"
+    echo "Node.js application is up and serving the expected version: $EXPECTED_VERSION"
   else
-    echo "Server is either down or not serving the expected version"
+    echo "Node.js application is either down or not serving the expected version"
   fi
 }
 
-# Call the function to check the server
+
 check_server
+
 
